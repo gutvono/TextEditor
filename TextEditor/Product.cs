13df456213d5f8a0b5f7e1edf81390578668778d
Gustavo <guvono4@gmail.com>
2024-05-07 15:46:23 -0300

    update

---
+++ TextEditor/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroProduto
+{
+    internal class Product
+    {
+        int id;
+        string description;
+        double price;
+        int quantity;
+
+        public Product(int id, string description, double price, int quantity)
+        {
+            this.Id = id;
+            this.Description = description;
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public override string ToString()
+        {
+            return $"{id};{Description};{Price};{Quantity}";
+        }
+    }
+}
--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -1,18 +1,90 @@
-﻿string path = @"C:\Dados\", file = "arquivo";
+﻿namespace CadastroProduto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Dados\Estoque\", file = "products.txt";
 
-if (!Directory.Exists(path))
-    Directory.CreateDirectory(path);
+            Product CreateProduct()
+            {
+                Console.WriteLine("----- CRIAR PRODUTO -----");
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("\nDescrição: ");
+                string description = Console.ReadLine();
+                Console.Write("\nPreço: ");
+                double price = double.Parse(Console.ReadLine());
+                Console.Write("\nQuantidade: ");
+                int quantity = int.Parse(Console.ReadLine());
 
-if (!File.Exists(path + file))
-    File.Create(path + file);
+                return new Product(id, description, price, quantity);
+            }
 
-StreamReader sr = new StreamReader(path + file);
-Console.WriteLine(sr.ReadToEnd());
-sr.Close();
+            List<Product> ProductList = new();
+            ProductList.Add(CreateProduct());
 
-Console.WriteLine();
+            void ShowAll(List<Product> list)
+            {
+                foreach (Product product in list) { Console.WriteLine(product.ToString()); }
+            }
 
-StreamReader sr2 = new StreamReader(path + file);
-//Console.WriteLine(File.ReadLines(path + file).Skip(2).Take(1).First());
-Console.WriteLine(File.ReadLines(path + file).ElementAt(2));
-sr2.Close();
+            ShowAll(ProductList);
+
+            ProductList.Add(CreateProduct());
+
+            ShowAll(ProductList);
+
+            SaveFile(ProductList, path, file);
+
+            void SaveFile(List<Product> list, string p, string f)
+            {
+                if (CheckIfExists(p, f))
+                {
+                    StreamWriter sw = new(p + f);
+
+                    foreach (Product product in list)
+                    {
+                        sw.WriteLine(product.ToString());
+                    }
+
+                    sw.Close();
+
+                    Console.WriteLine("Seu novo arquivo salvo:\n\n");
+                    StreamReader sr = new(p + f);
+                    sr.ReadToEnd();
+                    sr.Close();
+                }
+            }
+
+            bool CheckIfExists(string p, string f)
+            {
+                if (!Directory.Exists(p)) Directory.CreateDirectory(p);
+                if (!File.Exists(p + f)) File.Create(p + f);
+
+                return true;
+            }
+
+            List<Product> LoadFile(string p, string f)
+            {
+                List<Product> list = new();
+                if (CheckIfExists(p, f))
+                {
+                    foreach (string line in File.ReadAllLines(p + f))
+                    {
+                        string[] props;
+
+                        props = line.Split(";");
+                        int id = int.Parse(props[0]);
+                        string description = props[1];
+                        double price = double.Parse(props[2]);
+                        int quantity = int.Parse(props[3]);
+
+                        list.Add(new Product(id, description, price, quantity));
+                    }
+                }
+                return list;
+            }
+        }
+    }
+}
