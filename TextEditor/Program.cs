1b44868c77568a8a44df08ccb4db908dbfa26c4e
Gustavo <guvono4@gmail.com>
2024-05-07 13:43:19 -0300

    update

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -16,23 +16,3 @@ StreamReader sr2 = new StreamReader(path + file);
 //Console.WriteLine(File.ReadLines(path + file).Skip(2).Take(1).First());
 Console.WriteLine(File.ReadLines(path + file).ElementAt(2));
 sr2.Close();
-
-
-//if (File.Exists(path + file))
-//{
-//}
-
-//StreamWriter sw = new(path + file);
-
-//Console.WriteLine("Informe seu nome:");
-//sw.WriteLine(Console.ReadLine());
-
-//Console.WriteLine("Informe seu email:");
-//sw.WriteLine(Console.ReadLine());
-//sw.Close();
-
-//StreamReader sr = new (path + file);
-
-//Console.Clear();
-//Console.WriteLine(sr.ReadToEnd());
-//sr.Close();
